691567e4ff0eeaf228fb36c255b979eecd29db48
Alexander Smirnov <anari6754@gmail.com>
2018-06-04 21:05:37 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,33 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import { Route, Switch } from 'react-router';
-import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
-import createHistory from 'history/createBrowserHistory';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
+import App from './containers/app';
 
-import './index.css';
-import App from './containers/App/App';
-import SignUp from './containers/SignUp/SignUp';
-import SignIn from './containers/SignIn/SignIn';
-import reducer from './reducers';
+import './index.css'
 
-const history = createHistory();
-const middleware = routerMiddleware(history);
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(middleware)));
+const target = document.querySelector('#root');
 
-ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <div>
-                <Route exact path="/" component={App}/>
-                <Route path="/signin" component={SignIn}/>
-                <Route path="/signup" component={SignUp}/>
-            </div>
-        </ConnectedRouter>
-    </Provider>,
-    document.getElementById('root')
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  target
 );
